a04c2ee520148450f531bf0c4964bc1a210709c2
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-07-06 18:53:29 +0200

    Wikipedia module

---
+++ PublishedModules/Psychokiller1888/Wikipedia/Wikipedia.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+from wikipedia import wikipedia
+import core.base.Managers as managers
+from core.base.model.Intent import Intent
+from core.base.model.Module import Module
+from core.dialog.model.DialogSession import DialogSession
+
+
+class Wikipedia(Module):
+	"""
+	Author: Psychokiller1888
+	Description: Allows one to find informations about a topic on wikipedia
+	"""
+
+	_INTENT_SEARCH 			= Intent('DoSearch')
+	_INTENT_USER_ANSWER 	= Intent('UserRandomAnswer', isProtected=True)
+	_INTENT_SPELL_WORD 		= Intent('SpellWord', isProtected=True)
+
+	def __init__(self):
+		self._SUPPORTED_INTENTS	= [
+			self._INTENT_SEARCH,
+			self._INTENT_USER_ANSWER,
+			self._INTENT_SPELL_WORD
+		]
+
+		super(Wikipedia, self).__init__(self._SUPPORTED_INTENTS)
+
+
+	def onMessage(self, intent: str, session: DialogSession) -> bool:
+		if not self.filterIntent(intent, session):
+			return False
+
+		siteId = session.siteId
+		slots = session.slots
+		sessionId = session.sessionId
+		customData = session.customData
+
+		if intent == self._INTENT_SEARCH or session.previousIntent == self._INTENT_SEARCH:
+			if 'userInput' not in customData and 'what' not in slots:
+				if 'engine' not in slots:
+					engine = 'wikipedia'
+				else:
+					engine = slots['engine']
+
+				managers.MqttServer.continueDialog(
+					sessionId = sessionId,
+					text = managers.TalkManager.randomTalk(self.name, 'whatToSearch'),
+					intentFilter = [self._INTENT_USER_ANSWER],
+					previousIntent = self._INTENT_SEARCH,
+					customData = json.dumps({
+						'module': self.name,
+						'engine': engine
+					})
+				)
+			else:
+				if 'userInput' in customData:
+					what = customData['userInput']
+				elif 'what' in slots:
+					what = slots['what']
+				else:
+					managers.MqttServer.endTalk(sessionId=sessionId, text=managers.TalkManager.randomTalk('system', 'error'))
+					return True
+
+				if intent == self._INTENT_SPELL_WORD:
+					search = ''
+					for slot in session.slotsAsObjects['Letters']:
+						search += slot.value['value']
+				else:
+					search = what
+
+				wikipedia.set_lang(managers.LanguageManager.activeLanguage)
+				if 'engine' in customData:
+					engine = customData['engine']
+				else:
+					engine = 'wikipedia'
+
+				try:
+					if engine == 'wikipedia':
+						result = wikipedia.summary(search, sentences=3)
+					else:
+						result = wikipedia.summary(search, sentences=3)
+				except wikipedia.DisambiguationError:
+					managers.MqttServer.continueDialog(
+						sessionId = sessionId,
+						text = managers.TalkManager.randomTalk(self.name, 'ambiguous').format(search),
+						intentFilter = [self._INTENT_USER_ANSWER],
+						previousIntent = self._INTENT_SEARCH,
+						customData = json.dumps({
+							'module': self.name,
+							'engine': engine
+						})
+					)
+					return True
+				except wikipedia.WikipediaException:
+					managers.MqttServer.continueDialog(
+						sessionId = sessionId,
+						text = managers.TalkManager.randomTalk(self.name, 'noMatch').format(search),
+						intentFilter = [self._INTENT_USER_ANSWER],
+						previousIntent = self._INTENT_SEARCH,
+						customData = json.dumps({
+							'module': self.name,
+							'engine': engine
+						})
+					)
+					return True
+				except Exception as e:
+					self._logger.error('Error: {}'.format(e))
+					managers.MqttServer.endTalk(sessionId=sessionId, text=managers.TalkManager.randomTalk('system', 'error'))
+					return True
+
+				if result == '':
+					managers.MqttServer.continueDialog(
+						sessionId = sessionId,
+						text = managers.TalkManager.randomTalk(self.name, 'noMatch').format(search),
+						intentFilter = [self._INTENT_USER_ANSWER],
+						previousIntent = self._INTENT_SEARCH,
+						customData = json.dumps({
+							'module': self.name,
+							'engine': engine
+						})
+					)
+				else:
+					managers.MqttServer.endTalk(sessionId = sessionId, text = result, client = siteId)
+
+		return True
---
+++ PublishedModules/Psychokiller1888/Wikipedia/readme.md
@@ -0,0 +1,15 @@
+# Wikipedia
+
+### Download
+`wget http://bit.ly/PAWikipedia -O ~/ProjectAlice/system/moduleInstallTickets/Wikipedia.install`
+
+### Description
+Allows one to find informations about a topic on wikipedia
+
+- Version: 0.1
+- Author: Psychokiller1888
+- Maintainers: N/A
+- Alice minimum version: N/A
+- Conditions:
+  - en
+- Requirements: wikipedia
