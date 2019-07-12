7466f889b93f8af8e31dab569780d7e44e5731e0
mjlill <mjlill@cox.net>
2019-07-11 20:21:48 -0400

    corrected url link

--- PublishedModules/mjlill/LocalButtonPress/LocalButtonPress.py
+++ PublishedModules/mjlill/LocalButtonPress/LocalButtonPress.py
@@ -7,21 +7,20 @@ from core.base.model.Intent import Intent
 from core.base.model.Module import Module
 from core.dialog.model.DialogSession import DialogSession
 
+class LocalButtonPress(Module):
+	"""
+	Author: mjlill
+	Description: Press an imaginary button on or off
+	"""
 
-class Localbuttonpress(Module):
-    """
-    Author: mjlill
-    Description: Press an imaginary button on or off
-    """
-
-        _INTENT_BUTTON_ON        = Intent('DoButtonOn')
-        _INTENT_BUTTON_OFF       = Intent('DoButtonOff')
-        _LIGHT_PIN               = 4
+	_INTENT_BUTTON_ON        = Intent('DoButtonOn')
+	_INTENT_BUTTON_OFF       = Intent('DoButtonOff')
+	_LIGHT_PIN               = 4
 
 	def __init__(self):
 		self._SUPPORTED_INTENTS	= [
-                        self._INTENT_BUTTON_ON,
-                        self._INTENT_BUTTON_OFF
+			self._INTENT_BUTTON_ON,
+			self._INTENT_BUTTON_OFF
 		]
 
 		super(Localbuttonpress, self).__init__(self._SUPPORTED_INTENTS)
@@ -34,17 +33,17 @@ class Localbuttonpress(Module):
 		sessionId = session.sessionId
 		siteId = session.siteId
 
-                if intent == self._INTENT_BUTTON_ON:
+		if intent == self._INTENT_BUTTON_ON:
 
-                        GPIO.output(_LIGHT_PIN, GPIO.HIGH)
+			GPIO.output(_LIGHT_PIN, GPIO.HIGH)
 
-                        managers.MqttServer.endTalk(sessionId, managers.TalkManager.randomTalk(self.name, 'DoButtonOn'), client=siteId)
+			managers.MqttServer.endTalk(sessionId, managers.TalkManager.randomTalk(self.name, 'DoButtonOn'), client=siteId)
 
 
-                elif intent == self._INTENT_BUTTON_OFF:
+		elif intent == self._INTENT_BUTTON_OFF:
 
-                        GPIO.output(_LIGHT_PIN, GPIO.LOW)
+			GPIO.output(_LIGHT_PIN, GPIO.LOW)
 
-                        managers.MqttServer.endTalk(sessionId, managers.TalkManager.randomTalk(self.name,'DoButtonOff'), client=siteId)
+			managers.MqttServer.endTalk(sessionId, managers.TalkManager.randomTalk(self.name,'DoButtonOff'), client=siteId)
 
 		return True
