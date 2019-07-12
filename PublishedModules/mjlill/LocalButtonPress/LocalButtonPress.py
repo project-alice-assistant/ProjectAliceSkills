95821c2cfb9da5ebe3a7fa9e89b1fe6f7fa21718
mjlill <mjlill@cox.net>
2019-07-12 10:28:58 -0400

    add gpio

--- PublishedModules/mjlill/LocalButtonPress/LocalButtonPress.py
+++ PublishedModules/mjlill/LocalButtonPress/LocalButtonPress.py
@@ -7,44 +7,42 @@ from core.base.model.Intent import Intent
 from core.base.model.Module import Module
 from core.dialog.model.DialogSession import DialogSession
 
-
 class LocalButtonPress(Module):
-	"""
-	Author: mjlill
-	Description: Press an imaginary button on or off
-	"""
+    """
+    Author: mjlill
+    Description: Press an imaginary button on or off
+    """
 
-	_INTENT_BUTTON_ON        = Intent('DoButtonOn')
-	_INTENT_BUTTON_OFF       = Intent('DoButtonOff')
-	_LIGHT_PIN               = 4
+    _INTENT_BUTTON_ON   = Intent('DoButtonOn')
+    _INTENT_BUTTON_OFF      = Intent('DoButtonOff')
+    _LIGHT_PIN         = 4
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(4, GPIO.OUT)
+    GPIO.output(4, GPIO.LOW)
 
-	def __init__(self):
-		self._SUPPORTED_INTENTS	= [
-			self._INTENT_BUTTON_ON,
-			self._INTENT_BUTTON_OFF
-		]
-
-		super(Localbuttonpress, self).__init__(self._SUPPORTED_INTENTS)
-
-
-	def onMessage(self, intent: str, session: DialogSession) -> bool:
-		if not self.filterIntent(intent, session):
-			return False
-
-		sessionId = session.sessionId
-		siteId = session.siteId
-
-		if intent == self._INTENT_BUTTON_ON:
-
-			GPIO.output(_LIGHT_PIN, GPIO.HIGH)
-
-			managers.MqttServer.endTalk(sessionId, managers.TalkManager.randomTalk(self.name, 'DoButtonOn'), client=siteId)
+    def __init__(self):
+        self._SUPPORTED_INTENTS   = [
+            self._INTENT_BUTTON_ON,
+            self._INTENT_BUTTON_OFF
+        ]
+        super().__init__(self._SUPPORTED_INTENTS)
 
 
-		elif intent == self._INTENT_BUTTON_OFF:
+    def onMessage(self, intent: str, session: DialogSession) -> bool:
+        if not self.filterIntent(intent, session):
+            return False
 
-			GPIO.output(_LIGHT_PIN, GPIO.LOW)
+        sessionId = session.sessionId
+        siteId = session.siteId
 
-			managers.MqttServer.endTalk(sessionId, managers.TalkManager.randomTalk(self.name,'DoButtonOff'), client=siteId)
+        if intent == self._INTENT_BUTTON_ON:
+#            GPIO.output(_LIGHT_PIN, GPIO.HIGH)
+            GPIO.output(4, GPIO.HIGH)
+            managers.MqttServer.endTalk(sessionId, managers.TalkManager.randomTalk(self.name, 'DoButtonOn'), client=siteId)
 
-		return True
+        elif intent == self._INTENT_BUTTON_OFF:
+#            GPIO.output(_LIGHT_PIN, GPIO.LOW)
+            GPIO.output(4, GPIO.LOW)
+            managers.MqttServer.endTalk(sessionId, managers.TalkManager.randomTalk(self.name,'DoButtonOff'), client=siteId)
+
+        return True
--- PublishedModules/mjlill/LocalButtonPress/README.md
+++ PublishedModules/mjlill/LocalButtonPress/README.md
@@ -7,15 +7,15 @@ Press an imaginary button on or off
  The Google AIY hat has 500ma drivers on 4 gpio pins and I use one to turn on a work space light.
  It would be nice to ask Alice to do that for me and this is my attempt to put all the pieces
  together for a module. This can use any free gpio for a non AIY system, just use a resistor and a led. 
- edit the python script to assign the gpio pin
+ Edit the python script to assign a different gpio pin
 
 
-- Version: 1.0
+- Version: 1.1
 - Author: mjlill
 - Maintainers: N/A
 - Alice minimum version: N/A
 - Conditions:
   - en
 
-- Requirements: N/A
+- Requirements: RPI.GPIO
 
