4de8517697c5b9791ed8bc6a92146d8ada2fe7e2
maxbachmann <kontakt@maxbachmann.de>
2019-07-13 15:57:23 +0200

    add whitespace pre-commit hook

---
+++ setup/install-pre-commit.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# This script installs a pre-commit hook to enforce the correct use of spaces and tabs for this project
+
+GIT_DIR=`git rev-parse --git-common-dir 2> /dev/null`
+
+if [ "$GIT_DIR" == "" ]; then
+  echo "This does not appear to be a git repo."
+  exit 1
+fi
+
+if [ -f "$GIT_DIR/hooks/pre-commit" ]; then
+  echo "There is already a pre-commit hook installed. Delete it first."
+  echo
+  echo "    rm '$GIT_DIR/hooks/pre-commit'"
+  echo
+  exit 2
+fi
+
+cp $GIT_DIR/../pre-commit.hook $GIT_DIR/hooks/pre-commit
+
+echo
+echo "You're all set! Happy hacking!"
+
+exit 0
