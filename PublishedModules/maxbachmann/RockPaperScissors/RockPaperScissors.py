376476cd9270b2baec88aecf4c8b420df4bce0ae
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-08-13 22:29:01 +0200

    add rock paper scissors (#20)

---
+++ PublishedModules/maxbachmann/RockPaperScissors/RockPaperScissors.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import core.base.Managers as managers
+from core.base.model.Intent import Intent
+from core.base.model.Module import Module
+from core.dialog.model.DialogSession import DialogSession
+
+
+class RockPaperScissors(Module):
+	"""
+	Author: maxbachmann
+	Description: Play rock paper scissors
+	"""
+
+	_INTENT_ROCK_PAPER_SCISSORS = Intent('RockPaperScissors')
+
+	def __init__(self):
+		self._SUPPORTED_INTENTS = [
+			self._INTENT_ROCK_PAPER_SCISSORS
+		]
+
+		super().__init__(self._SUPPORTED_INTENTS)
+
+	def onMessage(self, intent: str, session: DialogSession) -> bool:
+		if not self.filterIntent(intent, session):
+			return False
+
+		if intent == self._INTENT_ROCK_PAPER_SCISSORS:
+			randomItem = self.randomTalk(text='RockPaperScissors')
+			managers.MqttServer.endTalk(session.sessionId, self.randomTalk(text='answer', replace=randomItem))
+
+		return True
