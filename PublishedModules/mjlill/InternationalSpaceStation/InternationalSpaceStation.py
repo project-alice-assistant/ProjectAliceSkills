75f89c258e74c4295291a74e02b97f89b9200ea6
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-08-13 21:59:23 +0200

    add space station module

---
+++ PublishedModules/mjlill/InternationalSpaceStation/InternationalSpaceStation.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+
+import core.base.Managers as managers
+from core.base.model.Intent import Intent
+from core.base.model.Module import Module
+from core.dialog.model.DialogSession import DialogSession
+
+
+class InternationalSpaceStation(Module):
+	"""
+	Author: mjlill
+	Description: Inquire information about the international space station
+	"""
+
+	_INTENT_ASTRONAUTS = Intent('Astronauts')
+	_INTENT_ISS_POSITION = Intent('IssPosition')
+
+	def __init__(self):
+		self._SUPPORTED_INTENTS	= [
+			self._INTENT_ASTRONAUTS,
+			self._INTENT_ISS_POSITION
+		]
+
+		super().__init__(self._SUPPORTED_INTENTS)
+
+	def onMessage(self, intent: str, session: DialogSession) -> bool:
+		if not self.filterIntent(intent, session):
+			return False
+
+		sessionId = session.sessionId
+
+		try:
+			if intent == self._INTENT_ASTRONAUTS:
+				text = self.getAstronauts()
+			elif intent == self._INTENT_ISS_POSITION:
+				text = self.getIssPosition()
+
+			managers.MqttServer.endTalk(sessionId, text=text)
+		except Exception as e:
+			self._logger.error(e)
+			managers.MqttServer.endTalk(sessionId,
+										text=managers.TalkManager.randomTalk(module=self.name, talk='noServer'))
+
+		return True
+
+	def queryApi(self, url: str) -> dict:
+		return requests.get(url=url).json
+
+	def getIssPosition(self) -> str:
+		data = self.queryApi('http://api.open-notify.org/iss-now.json')
+		location = data['iss_position']
+		return self.randomTalk(text='issPosition', replace=[
+				float(location['latitude']),
+				float(location['longitude'])
+			])
+
+	def getAstronauts(self) -> str:
+		data = self.queryApi('http://api.open-notify.org/astros.json')
+		amount = data['number']
+
+		if not amount:
+			return self.randomTalk(text='noAstronauts')
+		if amount == 1:
+			return self.randomTalk(text='oneAstronaut', replace=[data['people'][0]['name']])
+		return self.randomTalk(text='multipleAstronauts', replace=[
+				', '.join(str(x['name']) for x in data['people'][:-1]),
+				data['people'][-1]['name'],
+				amount
+			])
+		
---
+++ PublishedModules/mjlill/InternationalSpaceStation/README.md
@@ -0,0 +1,26 @@
+# InternationalSpaceStation
+
+### Download
+
+##### > WGET method
+```bash
+wget http://bit.ly/????????? -O ~/ProjectAlice/system/moduleInstallTickets/InternationalSpaceStation.install
+```
+
+##### > Alice CLI method
+```bash
+alice module:install mjlill/InternationalSpaceStation
+```
+
+### Description
+Inquire information about the international space station
+
+- Version: 0.2
+- Author: mjlill
+- Maintainers: maxbachmann
+- Alice minimum version: N/A
+- Conditions:
+  - en
+  - de
+- Requirements: N/A
+
