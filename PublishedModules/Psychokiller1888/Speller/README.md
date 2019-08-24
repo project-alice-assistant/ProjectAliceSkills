728d463d158686e6ad334194eed3482d27305833
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-08-24 19:02:24 +0200

    Speller module

---
+++ PublishedModules/Psychokiller1888/Speller/README.md
@@ -0,0 +1,28 @@
+# Speller
+
+### Download
+
+##### > WGET method
+```bash
+wget http://bit.ly/????????? -O ~/ProjectAlice/system/moduleInstallTickets/Speller.install
+```
+
+##### > Alice CLI method
+```bash
+alice module:install Psychokiller1888/Speller
+```
+
+### Description
+Ask alice how to spell any word!
+
+- Version: 0.1
+- Author: Psychokiller1888
+- Maintainers: N/A
+- Alice minimum version: N/A
+- Conditions:
+  - en
+  - fr
+  - asrArbitraryCapture
+- Pip requirements: N/A
+- System requirements: N/A
+
---
+++ PublishedModules/Psychokiller1888/Speller/Speller.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+import core.base.Managers as managers
+from core.base.model.Intent import Intent
+from core.base.model.Module import Module
+from core.dialog.model.DialogSession import DialogSession
+
+
+class Speller(Module):
+	"""
+	Author: Psychokiller1888
+	Description: Ask alice how to spell any word!
+	"""
+
+	_INTENT_DO_SPELL = Intent('DoSpellWord')
+	_INTENT_ANSWER_WORD = Intent('UserRandomAnswer', isProtected=True)
+
+	def __init__(self):
+		self._SUPPORTED_INTENTS	= [
+			self._INTENT_DO_SPELL,
+			self._INTENT_ANSWER_WORD
+		]
+
+		super().__init__(self._SUPPORTED_INTENTS)
+
+
+	def onMessage(self, intent: str, session: DialogSession) -> bool:
+		if not self.filterIntent(intent, session):
+			return False
+
+		sessionId = session.sessionId
+		slots = session.slots
+
+		if intent == self._INTENT_DO_SPELL or (session.previousIntent == self._INTENT_DO_SPELL and intent == self._INTENT_ANSWER_WORD):
+			if 'RandomWord' in slots and slots['RandomWord'] != 'unknownword':
+				word = slots['RandomWord']
+				string = '<break time="160ms"/>'.join(list(word))
+
+				managers.MqttServer.endTalk(
+					sessionId=sessionId,
+					text=self.randomTalk(
+						text='isSpelled',
+						replace=[word, string]
+					)
+				)
+			else:
+				managers.MqttServer.continueDialog(
+					sessionId=sessionId,
+					text=self.randomTalk('notUnderstood'),
+					intentFilter=[self._INTENT_ANSWER_WORD],
+					previousIntent=self._INTENT_DO_SPELL
+				)
+
+		return True
