eb02ce4855f0d4db64dec4202ff98fd1512b6362
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-08-30 15:38:16 +0200

    add speedtest module

---
+++ PublishedModules/maxbachmann/Speedtest/Speedtest.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import core.base.Managers as managers
+from core.base.model.Intent import Intent
+from core.base.model.Module import Module
+from core.dialog.model.DialogSession import DialogSession
+import speedtest
+
+class Speedtest(Module):
+	"""
+	Author: maxbachmann
+	Description: run internet speed test
+	"""
+
+	_INTENT_SPEEDTEST = Intent('Speedtest')
+
+	def __init__(self):
+		self._SUPPORTED_INTENTS = [
+			self._INTENT_SPEEDTEST
+		]
+
+		super().__init__(self._SUPPORTED_INTENTS)
+
+	def onMessage(self, intent: str, session: DialogSession) -> bool:
+		if not self.filterIntent(intent, session):
+			return False
+
+		if intent == self._INTENT_SPEEDTEST:
+			try:
+				self.say(self.randomTalk(text='running'))
+				servers = []
+				speed = speedtest.Speedtest()
+				speed.get_servers(servers)
+				speed.get_best_server()
+				speed.download()
+				speed.upload(pre_allocate=False)
+				speed.results.share()
+				result = speed.results.dict()
+				downspeed = '{:.2f}'.format(result['download']/1000000)
+				upspeed = '{:.2f}'.format(result['upload']/1000000)
+				self.endDialog(session.sessionId, self.randomTalk(text='result', replace=[downspeed, upspeed]))
+			except:
+				self.endDialog(session.sessionId, self.randomTalk(text='failed'))
+		return True
