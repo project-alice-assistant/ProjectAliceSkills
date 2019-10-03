1a5a4dd1d7d986587b39264b01da9822a291cfb7
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2019-10-03 03:21:01 +0200

    adjusted ISS map to map projection type

--- PublishedModules/mjlill/InternationalSpaceStation/widgets/js/WorldMap.js
+++ PublishedModules/mjlill/InternationalSpaceStation/widgets/js/WorldMap.js
@@ -4,15 +4,11 @@
             datatype: 'json',
             url: 'http://api.open-notify.org/iss-now.json',
             success: function (data) {
-                let latitude = data['iss_position']['latitude'];
-                let longitude = data['iss_position']['longitude'];
-
-                let x = (longitude + 180) * ($('.WorldMap_map').width() / 360);
-
-                let siny = Math.sin(latitude * Math.PI / 180);
-                siny = Math.min(Math.max(siny, -0.9999), 0.9999);
-                let y = $('.WorldMap_map').height() * (Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI));
-
+                let latitude = parseFloat(data['iss_position']['latitude']);
+                let longitude = parseFloat(data['iss_position']['longitude']);
+                
+                let x = $('.WorldMap_map').width()  / 360 * ( longitude + 180 );
+                let y = $('.WorldMap_map').height() / 180 * ( -latitude + 90 ) ;
 
                 $('.WorldMap_satellite').css('top', y - 10);
                 $('.WorldMap_satellite').css('left', x - 15);
