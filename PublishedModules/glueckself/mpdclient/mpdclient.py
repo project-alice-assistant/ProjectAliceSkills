747d998c8c9c307a88db654ff982e3a864b04f73
Srdjan Markovic <sweb-ghub@markoviclan.info>
2019-10-06 19:35:36 +0200

    poll mpd server and handle disconnects and playback state

--- PublishedModules/glueckself/mpdclient/mpdclient.py
+++ PublishedModules/glueckself/mpdclient/mpdclient.py
@@ -1,9 +1,13 @@
 import mpdhelper
 
+from core.ProjectAliceExceptions import ModuleStartDelayed, ModuleStartingFailed
 from core.base.model.Intent import Intent
 from core.base.model.Module import Module
 from core.dialog.model.DialogSession import DialogSession
 from core.commons.commons import online
+
+#>>> mpd.status()
+#{'volume': '44', 'repeat': '0', 'random': '0', 'single': '0', 'consume': '0', 'playlist': '2', 'playlistlength': '13', 'mixrampdb': '0.000000', 'state': 'play', 'song': '3', 'songid': '4', 'time': '1:178', 'elapsed': '0.789', 'bitrate': '128', 'audio': '44100:24:2', 'nextsong': '4', 'nextsongid': '5'}
 
 class mpdclient(Module):
 	"""
@@ -32,23 +36,54 @@ class mpdclient(Module):
 		self._password = config.get('mpdPassword')
 		
 		self._mpdConnected = False
-		
-		if self._host:
-			self._connect()
-		else:
-			self._logger.warn(f'[{self.name}] MPD host not configured, not doing anything.')
-		
-	def _connect(self):
 		self._mpd = mpdhelper.MPDClient()
+		
+		if not self._host:
+			self._logger.warn(f'[{self.name}] MPD host not configured, not doing anything.')
+			return
+		
+		self._refreshTimer = self.ThreadManager.newTimer(interval=1, func=self._mpd_poll_status, args=self, autoStart=True)
+	
+	def _mpd_poll_status(self):
+		status = self._mpd.status()
+		self._mpd_process_status(status)
+		self._refreshTimer.start()
+		
+	def _mpd_process_status(self, status: dict):
+		if not status:
+			self._mpd.disconnect()
+			self._mpdConnected = False
+			self._connect()
+			return
+		
+		playbackStatus = False
+		if status['state'] == 'play':
+			playbackStatus = True
+		
+		try:
+			intents = list()
+			# when playing: stop, next, prev. when stopped: play
+			intents = [
+				{'intentId': 'mpdPlay', 'enable': not playbackStatus },
+				{'intentId': 'mpdStop', 'enable': playbackStatus },
+				{'intentId': 'mpdNext', 'enable': playbackStatus },
+				{'intentId': 'mpdPrev', 'enable': playbackStatus }
+			]
+			self.MqttManager.configureIntents(intents)
+		except Exception as e:
+			self._logger.warning(f'[{self.name}] Failed to update intents to match the mpd state: {e}')
+	
+	def _connect(self):
 		try:
 			self._mpd.connect(self._host, self._port, 5)
 		except:
 			self._logger.warn(f'[{self.name}] Failed to connect to mpd host {self._host}:{self._port}, retrying in 10s.')
-			self.ThreadManager.doLater(interval=10, func=self._connect)
 			return
 		
 		if self._password:
-			self._mpd.password(self._password)
+			if not self._mpd.password(self._password):
+				# handle invalid password
+				pass
 			
 		self._logger.info(f'[{self.name}] Connected to mpd host {self._host}:{self._port}')
 		self._mpdConnected = True
