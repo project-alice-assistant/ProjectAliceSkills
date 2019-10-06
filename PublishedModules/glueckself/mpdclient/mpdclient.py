b8ddbfa21e3ae023dee2d7bf22e1c8b34dcaca86
Srdjan Markovic <sweb-ghub@markoviclan.info>
2019-10-06 13:10:33 +0200

    fixed quotas, fstrings and camelcase

--- PublishedModules/glueckself/mpdclient/mpdclient.py
+++ PublishedModules/glueckself/mpdclient/mpdclient.py
@@ -26,38 +26,38 @@ class mpdclient(Module):
 
 		super().__init__(self._SUPPORTED_INTENTS)
 		
-		config = self.getConfig("mpdclient")
-		self._host = config.get("mpdHost")
-		self._port = config.get("mpdPort")
-		self._password = config.get("mpdPassword")
+		config = self.getConfig('mpdclient')
+		self._host = config.get('mpdHost')
+		self._port = config.get('mpdPort')
+		self._password = config.get('mpdPassword')
 		
-		self._mpd_connected = False
+		self._mpdConnected = False
 		
 		if self._host:
 			self._connect()
 		else:
-			self._logger.warn("[{self.name}] MPD host not configured, not doing anything.")
+			self._logger.warn(f'[{self.name}] MPD host not configured, not doing anything.')
 		
 	def _connect(self):
 		self._mpd = mpdhelper.MPDClient()
 		try:
 			self._mpd.connect(self._host, self._port, 5)
 		except:
-			self._logger.warn("[{self.name}] Failed to connect to mpd host {self._host}:{self._port}, retrying in 10s.")
+			self._logger.warn(f'[{self.name}] Failed to connect to mpd host {self._host}:{self._port}, retrying in 10s.')
 			self.ThreadManager.doLater(interval=10, func=self._connect)
 		
 		if self._password:
 			self._mpd.passwod(self._password)
 			
-		self._logger.info("[{self.name}] Connected to mpd host {self._host}:{self._port}")
-		self._mpd_connected=True
+		self._logger.info(f'[{self.name}] Connected to mpd host {self._host}:{self._port}')
+		self._mpdConnected=True
 		
 	def onMessage(self, intent: str, session: DialogSession) -> bool:
 		if not self.filterIntent(intent, session):
 			return False
 		
-		if not self._mpd_connected:
-			self.endDialog(sessionId=session.sessionId, text=self.randomTalk("notConnected"))
+		if not self._mpdConnected:
+			self.endDialog(sessionId=session.sessionId, text=self.randomTalk('notConnected'))
 			return True
 
 		if intent == self._INTENT_PLAY:
