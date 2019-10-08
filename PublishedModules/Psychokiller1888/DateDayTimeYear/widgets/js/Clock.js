6a528568bce6f7ee65111a0e70571405eb05e9aa
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-10-08 19:42:44 +0200

    Readding missing widgets

---
+++ PublishedModules/Psychokiller1888/DateDayTimeYear/widgets/js/Clock.js
@@ -0,0 +1,23 @@
+(function () {
+    let up = true;
+
+    function startTime() {
+        let today = new Date();
+        let h = String(today.getHours()).padStart(2, '0');
+        let m = String(today.getMinutes()).padStart(2, '0');
+
+        if (up) {
+            $('#DateDayTimeYear_clock').html(h + ':' + m);
+        } else {
+            $('#DateDayTimeYear_clock').html(h + ' ' + m);
+        }
+
+        up = !up;
+
+        setTimeout(function () {
+            startTime();
+        }, 500);
+    }
+
+    startTime();
+})();
