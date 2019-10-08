108ab84f960d006c10da26b32f78869c06c462a9
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-10-08 16:15:00 +0200

    Wrong indent

--- PublishedModules/mjlill/InternationalSpaceStation/widgets/js/WhosAboard.js
+++ PublishedModules/mjlill/InternationalSpaceStation/widgets/js/WhosAboard.js
@@ -1,11 +1,11 @@
 (function () {
-    $.ajax({
-        datatype: 'json',
-        url: 'http://api.open-notify.org/astros.json',
-        success: function (data) {
-            $.each(data['people'], function(key, val) {
-                $('#WhosAboard_astronaut_list').append('<li class="WhosAboard_astonaut_list_item"><span class="fa-li"><i class="fas fa-user-astronaut"></i></span>' + val['name'] + '</li>');
-            });
-        }
-    })
+	$.ajax({
+		datatype: 'json',
+		url: 'http://api.open-notify.org/astros.json',
+		success: function (data) {
+			$.each(data['people'], function (key, val) {
+				$('#WhosAboard_astronaut_list').append('<li class="WhosAboard_astonaut_list_item"><span class="fa-li"><i class="fas fa-user-astronaut"></i></span>' + val['name'] + '</li>');
+			});
+		}
+	})
 })();
