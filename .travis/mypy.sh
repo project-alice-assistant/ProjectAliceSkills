2da9def1d2621ddedef7e62046e28d83609d73a2
maxbachmann <kontakt@maxbachmann.de>
2019-10-09 23:57:09 +0200

    fix return code by using separate script

---
+++ .travis/mypy.sh
@@ -0,0 +1,18 @@
+ERROR=0
+for author in PublishedModules/*; do
+	GREEN='\033[0;32m'
+	RED='\033[0;31m'
+	NC='\033[0m' # No Color
+	echo "${author##*/}"
+	for module in ${author}/*; do
+		OUTPUT=$(find $module -name '*.py' | xargs mypy --pretty)
+		ERR=$?
+		if [ $ERR -eq 0 ]; then
+			printf "  ${module##*/}: ${GREEN}${OUTPUT}${NC}\n"
+		else
+			printf "  ${RED}${OUTPUT}${NC}\n"
+			ERROR=$ERR
+		fi
+	done
+done
+exit $ERROR
