364286347e26338ea120c17083247fc30df525da
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-10-16 19:53:32 +0200

    Fix and cleanup IcanhazdadjokeDotCom

--- PublishedModules/Psychokiller1888/IcanhazdadjokeDotCom/IcanhazdadjokeDotCom.py
+++ PublishedModules/Psychokiller1888/IcanhazdadjokeDotCom/IcanhazdadjokeDotCom.py
@@ -10,20 +10,19 @@ class IcanhazdadjokeDotCom(Module):
 	_INTENT_TELL_A_JOKE = Intent('TellAJoke')
 
 	def __init__(self):
-		self._INTENTS = {
-			self._INTENT_TELL_A_JOKE: self.jokeIntent
-		}
+		self._INTENTS = [
+			(self._INTENT_TELL_A_JOKE, self.jokeIntent)
+		]
 
 		super().__init__(self._INTENTS)
 
 
-	def offlineHandler(self, session: DialogSession, **kwargs) -> bool:
+	def offlineHandler(self, session: DialogSession, **_kwargs):
 		self.endDialog(session.sessionId, text=self.TalkManager.randomTalk('offline', module='system'))
-		return True
 
 
 	@online(offlineHandler=offlineHandler)
-	def jokeIntent(self, intent: str, session: DialogSession) -> bool:
+	def jokeIntent(self, session: DialogSession, **_kwargs):
 		url = 'https://icanhazdadjoke.com/'
 
 		headers = {
@@ -37,4 +36,3 @@ class IcanhazdadjokeDotCom(Module):
 			self.endDialog(session.sessionId, text=response.text)
 		else:
 			self.endDialog(session.sessionId, self.TalkManager.getrandomTalk('noJoke'))
-		return True
