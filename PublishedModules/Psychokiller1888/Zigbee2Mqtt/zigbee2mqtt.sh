a16192c0634dd483d6447507a6585dbf6e703ad2
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-10-17 14:03:10 +0200

    Missing service file

--- PublishedModules/Psychokiller1888/Zigbee2Mqtt/zigbee2mqtt.sh
+++ PublishedModules/Psychokiller1888/Zigbee2Mqtt/zigbee2mqtt.sh
@@ -4,3 +4,20 @@ git clone https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
 chown -R pi:pi /opt/zigbee2mqtt
 cd /opt/zigbee2mqtt
 npm install
+
+cat >> /etc/systemd/system/zigbee2mqtt.service <<EOL
+[Unit]
+Description=zigbee2mqtt
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/npm start
+WorkingDirectory=/opt/zigbee2mqtt
+StandardOutput=inherit
+StandardError=inherit
+Restart=always
+User=pi
+
+[Install]
+WantedBy=multi-user.target
+EOL
