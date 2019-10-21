aa9ca57ad60fd99d174a1ad56a499fc0feb58149
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-10-21 08:46:43 +0200

    use new alpha3 dialog mappings (#67)
    * use new intent mappings
    
    * fix argument order
    
    * fix import
    
    * improve readability
    
    * fix wrong function name
    
    * use alpha3 functions
    
    * require alpha3
    
    * require alpha3
    
    * some small refactoring
    
    * apply latest alpha3 changes

--- PublishedModules/philipp2310/BringShoppingList/BringShoppingList.py
+++ PublishedModules/philipp2310/BringShoppingList/BringShoppingList.py
@@ -4,6 +4,7 @@ from BringApi.BringApi import BringApi
 from core.base.model.Intent import Intent
 from core.base.model.Module import Module
 from core.dialog.model.DialogSession import DialogSession
+from core.util.Decorators import Decorators
 
 
 class BringShoppingList(Module):
@@ -24,56 +25,38 @@ class BringShoppingList(Module):
 
 
 	def __init__(self):
-		self._SUPPORTED_INTENTS = [
-			self._INTENT_ADD_ITEM,
-			self._INTENT_DEL_ITEM,
-			self._INTENT_READ_LIST,
-			self._INTENT_CHECK_LIST,
-			self._INTENT_DEL_LIST,
+		self._INTENTS = [
+			(self._INTENT_ADD_ITEM, self.addItemIntent),
+			(self._INTENT_DEL_ITEM, self.delItemIntent),
+			(self._INTENT_CHECK_LIST, self.checkListIntent),
+			(self._INTENT_READ_LIST, self.readListIntent),
+			(self._INTENT_DEL_LIST, self.delListIntent),
 			self._INTENT_CONF_DEL,
 			self._INTENT_ANSWER_SHOP,
-			self._INTENT_SPELL_WORD]
+			self._INTENT_SPELL_WORD
+		]
 
-		super().__init__(self._SUPPORTED_INTENTS)
+		self._INTENT_ANSWER_SHOP.dialogMapping = {
+			self._INTENT_ADD_ITEM: self.addItemIntent,
+			self._INTENT_DEL_ITEM: self.delItemIntent,
+			self._INTENT_CHECK_LIST: self.checkListIntent
+		}
+
+		self._INTENT_SPELL_WORD.dialogMapping = {
+			self._INTENT_ADD_ITEM: self.addItemIntent,
+			self._INTENT_DEL_ITEM: self.delItemIntent,
+			self._INTENT_CHECK_LIST: self.checkListIntent
+		}
+
+		self._INTENT_CONF_DEL.dialogMapping = {
+			'confDelList': self.confDelIntent
+		}
+
+		super().__init__(self._INTENTS)
 
 		# Get config values
 		self._uuid = self.getConfig('uuid')
 		self._uuidlist = self.getConfig('bringListUUID')
-
-
-	def onMessage(self, intent: str, session: DialogSession) -> bool:
-		"""handle all incoming messages"""
-
-		if intent == self._INTENT_ADD_ITEM or (intent in {self._INTENT_ANSWER_SHOP, self._INTENT_SPELL_WORD} and session.previousIntent == self._INTENT_ADD_ITEM):
-			#Add item to list
-			self.editList(session, intent, 'add', self._addItemInt)
-			return True
-		elif intent == self._INTENT_DEL_ITEM or (intent in {self._INTENT_ANSWER_SHOP, self._INTENT_SPELL_WORD} and session.previousIntent == self._INTENT_DEL_ITEM):
-			#Delete items from list
-			self.editList(session, intent, 'rem', self._deleteItemInt)
-			return True
-		elif intent == self._INTENT_READ_LIST:
-			self.readList(session)
-			return True
-		elif intent == self._INTENT_CHECK_LIST or (intent in {self._INTENT_ANSWER_SHOP, self._INTENT_SPELL_WORD} and session.previousIntent == self._INTENT_CHECK_LIST):
-			#check if item is in list
-			self.editList(session, intent, 'chk', self._checkListInt)
-			return True
-		elif intent == self._INTENT_DEL_LIST:
-			self.continueDialog(
-				sessionId=session.sessionId,
-				text=self.randomTalk('chk_del_all'),
-				intentFilter=[self._INTENT_CONF_DEL],
-				previousIntent=self._INTENT_DEL_LIST)
-			return True
-		elif session.previousIntent == self._INTENT_DEL_LIST and intent == self._INTENT_CONF_DEL:
-			if self.Commons.isYes(session):
-				self.endDialog(session.sessionId, text=self._deleteCompleteList())
-			else:
-				self.endDialog(session.sessionId, text=self.randomTalk('nodel_all'))
-			return True
-
-		return False
 
 
 	def _getBring(self) -> BringApi:
@@ -81,6 +64,7 @@ class BringShoppingList(Module):
 		return BringApi(self._uuid, self._uuidlist)
 
 
+	@Decorators.online
 	def _deleteCompleteList(self) -> str:
 		"""
 		perform the deletion of the complete list
@@ -144,35 +128,65 @@ class BringShoppingList(Module):
 		return found, missing
 
 
-	def _getShopItems(self, session: DialogSession, intent: str) -> list:
+	def _getShopItems(self, intent: str, session: DialogSession) -> list:
 		"""get the values of shopItem as a list of strings"""
-		items = list()
 		if intent == self._INTENT_SPELL_WORD:
 			item = ''.join([slot.value['value'] for slot in session.slotsAsObjects['Letters']])
-			items.append(item.capitalize())
-		else:
-			if 'shopItem' in session.slots:
-				for x in session.slotsAsObjects['shopItem']:
-					if x.value['value'] != "unknownword":
-						items.append(x.value['value'])
-		return items
+			return [item.capitalize()]
+		
+		items = [x.value['value'] for x in session.slotsAsObjects.get('shopItem', list()) if x.value['value'] != "unknownword"]
 
-
-	### INTENTS ###
-	def editList(self, session: DialogSession, intent: str, answer: str, action: Callable[[list], Tuple[list, list]]):
-		items = self._getShopItems(session, intent)
-		if items:
-			successfull, failed = action(items)
-			self.endDialog(session.sessionId, text=self._combineLists(answer, successfull, failed))
-		else:
+		if not items:
 			self.continueDialog(
 				sessionId=session.sessionId,
 				text=self.randomTalk(f'{answer}_what'),
 				intentFilter=[self._INTENT_ANSWER_SHOP, self._INTENT_SPELL_WORD],
-				previousIntent=intent)
+				currentDialogState=intent)
+		return items
 
 
-	def readList(self, session: DialogSession):
+	### INTENTS ###
+	def delListIntent(self, session: DialogSession, **_kwargs):
+		self.continueDialog(
+			sessionId=session.sessionId,
+			text=self.randomTalk('chk_del_all'),
+			intentFilter=[self._INTENT_CONF_DEL],
+			currentDialogState='confDelList')
+
+
+	def confDelIntent(self, session: DialogSession, **_kwargs):
+		if self.Commons.isYes(session):
+			self.endDialog(session.sessionId, text=self._deleteCompleteList())
+		else:
+			self.endDialog(session.sessionId, text=self.randomTalk('nodel_all'))
+
+
+	@Decorators.online
+	def addItemIntent(self, intent: str, session: DialogSession):
+		items = self._getShopItems(session, intent)
+		if items:
+			added, exist = self._addItemInt(items)
+			self.endDialog(session.sessionId, text=self._combineLists('add', added, exist))
+
+
+	@Decorators.online
+	def delItemIntent(self, intent: str, session: DialogSession):
+		items = self._getShopItems(session, intent)
+		if items:
+			removed, exist = self._deleteItemInt(items)
+			self.endDialog(session.sessionId, text=self._combineLists('rem', removed, exist))
+
+
+	@Decorators.online
+	def checkListIntent(self, intent: str, session: DialogSession):
+		items = self._getShopItems(session, intent)
+		if items:
+			found, missing = self._checkListInt(items)
+			self.endDialog(session.sessionId, text=self._combineLists('chk', found, missing))
+
+
+	@Decorators.online
+	def readListIntent(self, intent: str, session: DialogSession):
 		"""read the content of the list"""
 		items = self._getBring().get_items().json()['purchase']
 		itemlist = [item['name'] for item in items]
@@ -181,24 +195,11 @@ class BringShoppingList(Module):
 
 	#### List/Text operations
 	def _combineLists(self, answer: str, first: list, second: list) -> str:
-		"""
-		Combines two lists(if filled)
-		first+CONN+second
-		first
-		second
-		"""
-		strout = ''
-		if first:
-			strout = self._getTextForList(answer, first)
+		firstAnswer = self._getTextForList(answer, first) if first else ''
+		secondAnswer = self._getTextForList(f'{answer}_f', second) if second else ''
+		combinedAnswer = self.randomTalk('state_con', [firstAnswer, secondAnswer]) if first and second else ''
 
-		if second:
-			backup = strout  # don't overwrite added list... even if empty!
-			strout = self._getTextForList(f'{answer}_f', second)
-
-		if first and second:
-			strout = self.randomTalk('state_con', [backup, strout])
-
-		return strout
+		return combinedAnswer or firstAnswer or secondAnswer
 
 
 	def _getTextForList(self, pref: str, items: list) -> str:
@@ -208,5 +209,5 @@ class BringShoppingList(Module):
 		if len(items) == 1:
 			return self.randomTalk(f'{pref}_one', [items[0]])
 
-		value = self.randomTalk('gen_list', ['", "'.join(items[:-1]), items[-1]])
+		value = self.randomTalk('gen_list', [', '.join(items[:-1]), items[-1]])
 		return self.randomTalk(f'{pref}_multi', [value])
