41f48367d8089bfee3961fa11b915e0a1ceff183
maxbachmann <kontakt@maxbachmann.de>
2019-10-23 10:34:26 +0200

    small cleanup

--- PublishedModules/maxbachmann/News/News.py
+++ PublishedModules/maxbachmann/News/News.py
@@ -16,11 +16,11 @@ class News(Module):
 	_INTENT_NEWS = Intent('News')
 
 	def __init__(self):
-		self._SUPPORTED_INTENTS	= [
+		self._INTENTS	= [
 			(self._INTENT_NEWS: self.getNews)
 		]
 
-		super().__init__(self._SUPPORTED_INTENTS)
+		super().__init__(self._INTENTS)
 		self._apiKey = self.getConfig('apiKey')
 		self._region = self.getConfig('langCode')
 
@@ -35,5 +35,5 @@ class News(Module):
 		response = requests.get(url=f'https://newsapi.org/v2/top-headlines?country={region}&category={category}&pageSize={number}&apiKey={self._apiKey}')
 		response.raise_for_status()
 		articles = response.json()['articles']
-		answer = '<break time=\"400ms\"/>'.join([f"{article['title']}<break time=\"200ms\"/>{article['description']}" for article in articles])
+		answer = '<break time="400ms"/>'.join([f"{article['title']}<break time=\"200ms\"/>{article['description']}" for article in articles])
 		self.endDialog(sessionId=session.sessionId, text=answer)
