1c8cb6316f5e9067e68db5b9ff975645d18bb3d6
maxbachmann <kontakt@maxbachmann.de>
2019-12-19 23:08:03 +0100

    unsubscribe before sending

--- PublishedSkills/maxbachmann/HomeAssistant/HomeAssistant.py
+++ PublishedSkills/maxbachmann/HomeAssistant/HomeAssistant.py
@@ -1,3 +1,5 @@
+from paho.mqtt import client as MQTTClient
+
 from core.base.model.AliceSkill import AliceSkill
 from core.dialog.model.DialogSession import DialogSession
 from core.util.Decorators import MqttHandler
@@ -9,20 +11,33 @@ class HomeAssistant(AliceSkill):
 	Description: bridge events between alice and HomeAssistant
 	"""
 
+
 	def onEvent(self, event: str, **kwargs):
+		mqttClient = self.MqttManager.mqttClient.unsubscribe('projectalice/events/+')
 		self.MqttManager.publish(topic=f'projectalice/events/{event}', payload=kwargs)
+		mqttClient = self.MqttManager.mqttClient.subscribe('projectalice/events/+')
 
 
 	@MqttHandler('projectalice/events/+')
 	def incomingEventHandler(self, session: DialogSession):
 		eventName = session.intentName.replace('projectalice/events/', '')
 		payload = self.Commons.payload(session.message)
-		
+
 		try:
 			self.SkillManager.skillBroadcast(method=eventName, **payload)
 			argumentLog = ', '.join([f'{name}={value}' for name, value in payload.items()])
 			self.logInfo(f'broadcasting {eventName} with the arguments: {argumentLog}')
 		except TypeError:
 			self.logError(f'broadcasting {eventName} failed: The payload {payload} is not supported')
+
+
+	@MqttHandler('projectalice/dialog/say')
+	def sayDialogHandler(self, session: DialogSession):
+		payload = self.Commons.payload(session.message)
+		
+		try:
+			self.say(**payload):
+		except TypeError:
+			self.logError(f'incoming message say was ignored: The payload {payload} is not supported')
 
 
