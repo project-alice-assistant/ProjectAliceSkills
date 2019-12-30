3c8dfb6a22caaee30265a23ffc04c63e29642784
maxbachmann <kontakt@maxbachmann.de>
2019-12-30 18:22:48 +0100

    test environment variable

---
+++ .github/workflows/store.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import requests
+import json
+import os
+
+
+print(os.environ['RebrandlyApiKey'])
+clickCounts = requests.get(
+	'https://api.rebrandly.com/v1/links',
+	headers={
+		'Content-Type': 'application/json',
+		'apikey': os.environ['RebrandlyApiKey']
+	}
+)
+
+clickCounts = {skill['slashtag']: skill for skill in clickCounts.json()}
+
+skillStore = list()
+skillPath = Path('PublishedSkills')
+for installer in skillPath.glob('*/*/*.install'):
+	skillName = installer.stem
+	try:
+		downloads = clickCounts[skillName]['clicks']
+	except KeyError:
+		downloads = 0
+
+	with installer.open() as json_file:
+		data = json.load(json_file)
+		data['downloads'] = downloads
+		skillStore.append(data)
+
+storePath = Path('store/store.json')
+storePath.parent.mkdir(parents=True, exist_ok=True)
+storePath.touch()
+storePath.write_text(json.dumps(skillStore))
+
