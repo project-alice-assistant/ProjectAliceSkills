891c464627121e31ccbd7709968020c9a57e2a0e
maxbachmann <kontakt@maxbachmann.de>
2019-12-31 17:41:18 +0100

    add badges

---
+++ ci_sources/store.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import jinja2
+import requests
+import json
+import os
+
+
+	def createTemplateFile(self, outputPath: str, templateFile: str, **kwargs) -> str:
+		templateLoader = jinja2.FileSystemLoader(searchpath=os.path.join(os.path.dirname(__file__), 'templates'))
+		templateEnv = jinja2.Environment(loader=templateLoader, autoescape=True)
+		template = templateEnv.get_template(templateFile)
+		(self._skillPath / outputPath).write_text(template.render(**kwargs))
+
+clickCounts = requests.get(
+	'https://api.rebrandly.com/v1/links',
+	headers={
+		'Content-Type': 'application/json',
+		'apikey': os.environ['RebrandlyApiKey']
+	}
+)
+
+clickCounts = {skill['slashtag']: skill for skill in clickCounts.json()}
+
+skillStore = list()
+skillPath = Path('PublishedSkills')
+storePath = Path(__file__).parent.parent.resolve() / 'store'
+storePath.mkdir(parents=True, exist_ok=True)
+
+for installer in skillPath.glob('*/*/*.install'):
+	skillName = installer.stem
+	try:
+		downloads = clickCounts[skillName]['clicks']
+	except KeyError:
+		downloads = 0
+
+	templateLoader = jinja2.FileSystemLoader(searchpath=os.path.dirname(__file__))
+	templateEnv = jinja2.Environment(loader=templateLoader, autoescape=True)
+	template = templateEnv.get_template('badge.tmpl')
+	badgeFile = Path(storePath / f'{skillName}.svg')
+	badgeFile.write_text(template.render({'downloads': downloads}))
+
+	with installer.open() as json_file:
+		data = json.load(json_file)
+		data['downloads'] = downloads
+		skillStore.append(data)
+
+storeFile = (storePath / 'store.json')
+storeFile.write_text(json.dumps(skillStore))
