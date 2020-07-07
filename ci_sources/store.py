87c42d351c0131c1bf9b25e8a576b83f4f4ffafa
Philipp <41761223+philipp2310@users.noreply.github.com>
2020-07-07 21:08:37 +0200

    revert

--- ci_sources/store.py
+++ ci_sources/store.py
@@ -61,7 +61,7 @@ releaseTypes = {
 for releaseType, releaseName in releaseTypes.items():
 	for installer in skillPath.glob('*/*.install'):
 		skillName = installer.stem
-		print(f'{releaseName}-{skillName}:{installer["conditions"]}')
+		print(f'{releaseName}-{skillName}')
 		try:
 			downloads = clickCounts[skillName]
 		except KeyError:
