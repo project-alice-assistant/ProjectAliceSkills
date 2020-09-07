90595e520307460ad52bc173e7cc68bc07eed489
Psycho <laurentchervet@bluewin.ch>
2020-09-07 06:41:19 +0200

    :sparkles: bulk update script for submodules

---
+++ PublishedSkills/bulkUpdate.bat
@@ -0,0 +1,16 @@
+@echo off
+echo "Be aware that this script will checkout the master of every skills, stash your changes and pull"
+echo "latest changes before trying to reapply your changes which might fail but no rollback is available!"
+pause
+set back=%cd%
+for /d %%i in (D:\repositories\git\ProjectAlice\ProjectAliceSkills\PublishedSkills\*) do (
+	cd "%%i"
+	echo current directory: %%i
+	git -C %%i checkout master
+	git -C %%i stash
+	git -C %%i remote prune origin
+	git -C %%i pull
+	git -C %%i stash apply
+	cd
+)
+pause
