138f85694e40add4005802cace9c5da1fa2b0571
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-10-21 20:18:27 +0200

    :art: change the way skill store works

--- ci_sources/store.py
+++ ci_sources/store.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
-
-import jinja2
 import json
 import os
+from dataclasses import dataclass
+from pathlib import Path
+
 import requests
 from Version import Version
 from git import Repo
-from pathlib import Path
 
 
 @dataclass
@@ -25,20 +24,21 @@ class TagVersion:
 			Version.fromString(aliceMinVersion))
 
 
-clickCounts = {}
+clickCounts = dict()
 results = requests.get(
-	'https://api.rebrandly.com/v1/links',
+	url='https://api.rebrandly.com/v1/links',
 	headers={
 		'Content-Type': 'application/json',
 		'apikey'      : os.environ['RebrandlyApiKey']
 	}
 ).json()
+
 clicks = {skill['slashtag']: skill['clicks'] for skill in results}
 
 while clicks:
 	clickCounts.update(clicks)
 	results = requests.get(
-		f"https://api.rebrandly.com/v1/links?last={results[-1]['id']}",
+		url=f"https://api.rebrandly.com/v1/links?last={results[-1]['id']}",
 		headers={
 			'Content-Type': 'application/json',
 			'apikey'      : os.environ['RebrandlyApiKey']
@@ -51,62 +51,48 @@ skillPath = Path('PublishedSkills')
 storePath = Path(__file__).parent.parent.resolve() / 'store'
 storePath.mkdir(parents=True, exist_ok=True)
 
-releaseTypes = {
-	'a'      : 'alpha',
-	'b'      : 'beta',
-	'rc'     : 'rc',
-	'release': 'master'
-}
+for installer in skillPath.glob('*/*.install'):
+	skillName = installer.stem
+	print(f'Building for "{skillName}"')
 
-for releaseType, releaseName in releaseTypes.items():
-	for installer in skillPath.glob('*/*.install'):
-		skillName = installer.stem
-		print(f'{skillName} in {releaseName}')
-		try:
-			downloads = clickCounts[skillName]
-		except KeyError:
-			downloads = 0
+	skillRepo = Repo(installer.parent)
+	skillRepo.remote().fetch('--tags')
+	tags = [TagVersion.fromString(tag) for tag in skillRepo.tags if '_' in str(tag)]
 
-		templateLoader = jinja2.FileSystemLoader(searchpath=os.path.dirname(__file__))
-		templateEnv = jinja2.Environment(loader=templateLoader, autoescape=True)
-		template = templateEnv.get_template('badge.tmpl')
-		badgeFile = Path(storePath / f'{skillName}.svg')
-		badgeFile.write_text(template.render({'downloads': downloads}))
+	versions = dict()
+	while tags:
+		maxVersion = max(tags, key=lambda p: p.skillVersion)
+		tags = [tag for tag in tags if tag.aliceMinVersion < maxVersion.aliceMinVersion]
+		versions[str(maxVersion.aliceMinVersion)] = str(maxVersion.skillVersion)
 
-		skillRepo = Repo(installer.parent)
-		skillRepo.remote().fetch("--tags")
-		tags = [TagVersion.fromString(tag) for tag in skillRepo.tags if '_' in str(tag)]
-		tags = [tag for tag in tags if tag.skillVersion.releaseType >= releaseType]
-		versions = dict()
-		while tags:
-			maxVersion = max(tags, key=lambda p: p.skillVersion)
-			tags = [tag for tag in tags if tag.aliceMinVersion < maxVersion.aliceMinVersion]
-			versions[str(maxVersion.aliceMinVersion)] = str(maxVersion.skillVersion)
+	print(f'- Version mapping: {versions}')
 
-		print(f'- Version mapping: {versions}')
+	try:
+		downloads = clickCounts[skillName]
+	except KeyError:
+		downloads = 0
 
-		with installer.open() as json_file:
-			data = json.load(json_file)
-			data['downloads'] = downloads
-			data['versionMapping'] = versions
+	with installer.open() as json_file:
+		data = json.load(json_file)
+		data['downloads'] = downloads
+		data['versionMapping'] = versions
+		skillStore[data['name']] = data
 
-			skillStore[data['name']] = data
+print('Generating samples file')
+samples = dict()
+for sample in skillPath.rglob('*.sample'):
+	skillName = sample.parent.parent.stem
 
-	print('Generating samples file')
-	samples = dict()
-	for sample in skillPath.rglob('*.sample'):
-		skillName = sample.parent.parent.stem
+	print(f'Found {sample.stem}.sample for skill {skillName}')
 
-		print(f'Found {sample.stem}.sample for skill {skillName}')
+	language = sample.stem
 
-		language = sample.stem
+	intentsSamples = json.loads(sample.read_text())
+	samples.setdefault(str(skillName), dict())
+	samples[str(skillName)][language] = intentsSamples
 
-		intentsSamples = json.loads(sample.read_text())
-		samples.setdefault(str(skillName), dict())
-		samples[str(skillName)][language] = intentsSamples
+storeFile = (storePath / f'skills.json')
+storeFile.write_text(json.dumps(skillStore, ensure_ascii=False, indent='\t', sort_keys=True))
 
-	storeFile = (storePath / f'{releaseName}.json')
-	storeFile.write_text(json.dumps(skillStore, ensure_ascii=False, indent='\t', sort_keys=True))
-
-	sampleStoreFile = (storePath / f'{releaseName}.samples')
-	sampleStoreFile.write_text(json.dumps(samples, ensure_ascii=False, indent='\t', sort_keys=True))
+sampleStoreFile = (storePath / f'skills.samples')
+sampleStoreFile.write_text(json.dumps(samples, ensure_ascii=False, indent='\t', sort_keys=True))
