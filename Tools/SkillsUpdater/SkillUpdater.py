054918ee441688a7fb811db6dab8d9cbeea3f251
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-08-04 11:46:03 +0200

    bump refs

--- Tools/SkillsUpdater/SkillUpdater.py
+++ Tools/SkillsUpdater/SkillUpdater.py
@@ -17,32 +17,6 @@
 #
 #  Last modified: 2021.07.28 at 16:35:11 CEST
 
-#  Copyright (c) 2021
-#
-#  This file, SkillUpdater.py, is part of Project Alice.
-#
-#  Project Alice is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>
-#
-#  Last modified: 2021.04.13 at 12:49:04 CEST
-
-#  This file, SkillUpdater.py, is part of Project Alice.
-#
-#
-#
-#
-#  Last modified: 2020/10/2 18:41
-#  Last modified by: Psycho
 
 import json
 import os
